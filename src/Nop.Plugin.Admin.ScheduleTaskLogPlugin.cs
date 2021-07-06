f96ec179b6d538d7cc8b5fcb311ae73ef50cbbfc
Jamie Burns <evilbhonda@gmail.com>
2021-07-06 15:42:19 +0100

    Removed update method AB#213

--- src/Nop.Plugin.Admin.ScheduleTaskLogPlugin.cs
+++ src/Nop.Plugin.Admin.ScheduleTaskLogPlugin.cs
@@ -132,53 +132,6 @@ namespace Nop.Plugin.Admin.ScheduleTaskLog
             base.Install();
         }
 
-        public override void Update(string currentVersion, string targetVersion)
-        {
-            _localizationService.AddPluginLocaleResource(new Dictionary<string, string>
-            {
-                ["Plugins.Admin.ScheduleTaskLog.ListTitle"] = "Schedule task log",
-                ["Plugins.Admin.ScheduleTaskLog.ListViewTitle"] = "Schedule task log entry",
-                ["Plugins.Admin.ScheduleTaskLog.BackToTaskList"] = "back to schedule tasks",
-                ["Plugins.Admin.ScheduleTaskLog.BackToList"] = "back to schedule task log",
-                ["Plugins.Admin.ScheduleTaskLog.TaskName"] = "Task",
-                ["Plugins.Admin.ScheduleTaskLog.StartDate"] = "Start",
-                ["Plugins.Admin.ScheduleTaskLog.EndDate"] = "End",
-                ["Plugins.Admin.ScheduleTaskLog.TaskStatus"] = "Status",
-                ["Plugins.Admin.ScheduleTaskLog.TotalMilliseconds"] = "Time taken (milliseconds)",
-                ["Plugins.Admin.ScheduleTaskLog.ExceptionMessage"] = "Error message",
-                ["Plugins.Admin.ScheduleTaskLog.ExceptionDetails"] = "Error details",
-                ["Plugins.Admin.ScheduleTaskLog.TimeAgainstAverage"] = "Time against average (%)",
-                ["Plugins.Admin.ScheduleTaskLog.Success"] = "Success",
-                ["Plugins.Admin.ScheduleTaskLog.Error"] = "Error",
-                ["Plugins.Admin.ScheduleTaskLog.ClearLog"] = "Clear log",
-                ["Plugins.Admin.ScheduleTaskLog.ActivityLog.DeleteScheduleTaskLog"] = "Clear log",
-                ["Plugins.Admin.ScheduleTaskLog.List.ScheduleTaskId"] = "Schedule task",
-                ["Plugins.Admin.ScheduleTaskLog.List.ScheduleTaskId.Hint"] = "Select a schedule task.",
-                ["Plugins.Admin.ScheduleTaskLog.List.StateId"] = "State",
-                ["Plugins.Admin.ScheduleTaskLog.List.StateId.Hint"] = "Select a state.",
-                ["Plugins.Admin.ScheduleTaskLog.List.TriggerTypeId"] = "Trigger type",
-                ["Plugins.Admin.ScheduleTaskLog.List.TriggerTypeId.Hint"] = "Select a trigger type.",
-                ["Plugins.Admin.ScheduleTaskLog.List.StartedOnFrom"] = "Started date from",
-                ["Plugins.Admin.ScheduleTaskLog.List.StartedOnFrom.Hint"] = "The earliest start date for this search.",
-                ["Plugins.Admin.ScheduleTaskLog.List.StartedOnTo"] = "Started date to",
-                ["Plugins.Admin.ScheduleTaskLog.List.StartedOnTo.Hint"] = "The latest start date for this search.",
-                ["Plugins.Admin.ScheduleTaskLog.LogListLink"] = "View logs",
-                ["Plugins.Admin.ScheduleTaskLog.TriggerType"] = "Trigger type",
-                ["Plugins.Admin.ScheduleTaskLog.ByScheduler"] = "By scheduler",
-                ["Plugins.Admin.ScheduleTaskLog.ByUser"] = "By user",
-                ["Plugins.Admin.ScheduleTaskLog.TriggeredByCustomerEmail"] = "Triggered by customer email",
-                ["Plugins.Admin.ScheduleTaskLog.UnknownUser"] = "Unknown user",
-                ["Plugins.Admin.ScheduleTaskLog.Configuration.DisableLogs"] = "Pause logs",
-                ["Plugins.Admin.ScheduleTaskLog.Configuration.DisableLogs.Hint"] = "Disable collection of logs",
-                ["Plugins.Admin.ScheduleTaskLog.Configuration.LogExpiryDays"] = "Log expiry (in days)",
-                ["Plugins.Admin.ScheduleTaskLog.Configuration.LogExpiryDays.Hint"] = "How long logs should be valid for (in days), until they are eligible for pruning",
-                ["Plugins.Admin.ScheduleTaskLog.Configuration.CouldNotBeSaved"] = "The changes could not be saved",
-                ["Plugins.Admin.ScheduleTaskLog.Configuration.LogExpiryDays.MustBePositive"] = "The log expiry must be greater than 0"
-            });
-
-            base.Update(currentVersion, targetVersion);
-        }
-
         /// <summary>
         /// Uninstall the plugin
         /// </summary>
